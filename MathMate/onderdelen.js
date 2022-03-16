0ada3b27debe19243680da6c6544003e6c15eace
ahmedmohamoud <6008175@mborijnland.nl>
2022-03-16 10:06:18 +0100

    MathMate toegevoegd

---
+++ MathMate/onderdelen.js
@@ -0,0 +1,91 @@
+
+function myFunction() {
+    let y = document.getElementById("txt1").value;
+    let z = document.getElementById("txt2").value;
+    let x = +y + +z;
+    document.getElementById("demo").innerHTML = x;
+  }
+
+  function myFunction2() {
+    let y = document.getElementById("txt3").value;
+    let z = document.getElementById("txt4").value;
+    let x = y - z;
+    document.getElementById("demo1").innerHTML = x;
+  }
+
+  function myFunction3() {
+    let y = document.getElementById("txt5").value;
+    let z = document.getElementById("txt6").value;
+    let x = y * z;
+    document.getElementById("demo2").innerHTML = x;
+  }
+
+  function myFunction4() {
+    let y = document.getElementById("txt7").value;
+    let z = document.getElementById("txt8").value;
+    let x = y / z;
+    document.getElementById("demo3").innerHTML = x;
+  }
+
+  function myFunction5() {
+    let y = document.getElementById("txt9").value;
+    let z = document.getElementById("txt10").value;
+    let x = y ** z;
+    document.getElementById("demo4").innerHTML = x;
+  }
+
+  function myFunction6() {
+    let y = document.getElementById("txt11").value;
+    let z = document.getElementById("txt12").value;
+    let x = (y / 100) * z;
+    document.getElementById("demo5").innerHTML = x;
+  }
+
+  function myFunction7() {
+    let y = document.getElementById("txt13").value;
+    let z = document.getElementById("txt14").value;
+    let x = y % z;
+    document.getElementById("demo6").innerHTML = x;
+  }
+   
+ function myTafel(){ 
+  let table = document.getElementById("tafel1").value;
+  let length = document.getElementById("tafel2").value;
+  let i = 1;
+  let x = "<br>"+i+" * "+table+" = " +(i * table);
+
+
+for(i=1; i <= length; i++);
+document.getElementById("result1").innerHTML = x;
+
+
+
+
+
+  /*let number = document.getElementById("tafel2").value;
+   let l = document.getElementById("tafel1").value;
+   let x = console.log(`${number} * ${i} = ${result}`);
+   for(let i = 1; i <= l; i++) {
+
+    const result = i * number;
+    document.getElementById("result1").innerHTML = x;
+
+}*/
+
+}
+
+
+
+ 
+
+ function VindEven(){
+    
+    let num = document.getElementById('num').value;
+
+    
+    if ( num % 2 == 0) {
+        document.getElementById('result').innerHTML = num + ' is een Even nummer';
+    }else{
+        document.getElementById('result').innerHTML = num + ' is een Oneven nummmer';
+    }
+}     
