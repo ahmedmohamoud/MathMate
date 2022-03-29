c0a7547abf82d4fb87f3dfa4463fd75744fb1531
ahmedmohamoud <6008175@mborijnland.nl>
2022-03-29 14:10:37 +0200

    Update onderdelen.js

--- MathMate/onderdelen.js
+++ MathMate/onderdelen.js
@@ -91,12 +91,12 @@ function Kwadraten() {
 function Fibonacci() {  
 let n1 = 0,  n2 = 1, next_num, i;  
 let num = document.getElementById('num1').value;  
-for ( i = 1; i <= num; i++)  
-{  document.write (" <br> " +  n1); // print the n1  
-    next_num = n1 + n2; // sum of n1 and n2 into the next_num  
+for ( i = 1; i >= num; i++)  
+{  document.getElementById("result5").innerHTML +=" <br> " +  n1; 
+    next_num = n1 + n2; 
       
-    n1 = n2; // assign the n2 value into n2  
-    n2 = next_num; // assign the next_num into n2  
+    n1 = n2; 
+    n2 = next_num;
 }  
 }
 
